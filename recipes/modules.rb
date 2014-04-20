f269474cc260b313ee5125a35202492c2efbfa3a
Gennadiy Filatov <gfilatov@cpan.org>
2014-04-20 19:13:27 +0400

    install dependencies using package()

--- recipes/modules.rb
+++ recipes/modules.rb
@@ -1,5 +1,15 @@
 
 node[:perl][:modules].each do |spec|
+	unless spec[:deps].nil?
+		unless spec[:deps][:packages].nil?
+			spec[:deps][:packages].each do |pkg|
+				package pkg[:name] do
+					action :install
+				end
+			end
+		end
+	end
+
 	cpan_module spec[:name] do
 		version ( spec[:version] || '' )
 	end
